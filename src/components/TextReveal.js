7dcfdbafbf66fe200aae6b2fe45ea29a67f88f62
IvanSmiths <info@ivansmiths.com>
2023-03-06 19:17:46 +0100

    add jsx & array ref

---
+++ src/components/TextReveal.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+
+
+function TextReveal() {
+    const [lettersRef, setlettersRef] = useArrayRef();
+    const triggerRef = useRef(null)
+
+    const text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut."
+    gsap.registerPlugin(ScrollTrigger);
+
+    function useArrayRef() {
+        const lettersRef = useRef([]);
+        lettersRef.current = [];
+        return [lettersRef, (ref) => ref && lettersRef.current.push(ref)];
+    }
+
+  return (
+    <>
+    <div className="spacing-small"></div>
+        <div className="reveal">
+            <div ref={triggerRef}>
+                {text.split("").map((letter, index) => (
+                    <span className="reveal-text" key={index} ref={setlettersRef}>
+                        {letter}
+                    </span>
+                ))}
+            </div>
+        </div>
+    <div className="spacing"></div>
+    </>
+  )
+}
+
+export default TextReveal
